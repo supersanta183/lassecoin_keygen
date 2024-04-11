284d89d7839974bf27f5b981b0feb54e7d687db0
supersanta183 <klemmensen11@gmail.com>
2024-04-11 02:15:22 +0200

    changed main

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,5 @@ use keygen::Keygen;
 
 fn main() {
     let keygen = Keygen::new();
-    //let Keypair = keygen.generate_keypair_and_write_to_file();
-
-    let keygen = Keygen::new();
-    let seedphrase = keygen.generate_seedphrase();
-    println!("{}", seedphrase);
+    let Keypair = keygen.generate_keypair_and_write_to_file();
 }
