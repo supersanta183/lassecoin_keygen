1b68f11a8f76fcad4c3264c4e145f6d967986255
supersanta183 <klemmensen11@gmail.com>
2024-04-11 02:05:50 +0200

    made keygen struct

---
+++ src/keygen.rs
@@ -0,0 +1,78 @@
+use arrayref::array_ref;
+use bip39::{Language, Mnemonic, MnemonicType, Seed};
+use rand::{SeedableRng};
+use rand_chacha::ChaCha20Rng;
+use rsa::{pkcs1::EncodeRsaPublicKey, RsaPrivateKey, RsaPublicKey};
+use std::{fs};
+
+pub struct KeyPair {
+    priv_key: RsaPrivateKey,
+    pub pub_key: RsaPublicKey,
+}
+
+pub struct Keygen {}
+
+impl Keygen {
+    pub fn new() -> Self {
+        Self {}
+    }
+
+    // generates a mnemonic and writes the corresponding seedphrase to seedphrase.txt
+    pub fn generate_seedphrase(&self) -> String {
+        // create a new randomly generated mnemonic phrase
+        let mnemonic = Mnemonic::new(MnemonicType::Words12, Language::English);
+        let phrase = mnemonic.phrase().to_owned();
+        phrase
+    }
+
+    pub fn generate_rsa_keypair(&self, seedphrase: &String) -> Result<KeyPair, String> {
+        let mnemonic = Mnemonic::from_phrase(seedphrase.as_str(), Language::English).unwrap();
+        let seed = Seed::new(&mnemonic, "");
+        let seed_array = array_ref!(seed.as_bytes(), 0, 32);
+        let mut rng = ChaCha20Rng::from_seed(*seed_array);
+
+        let priv_key = match RsaPrivateKey::new(&mut rng, 2048) {
+            Ok(x) => x,
+            Err(e) => return Err(e.to_string())
+        };
+        let pub_key = RsaPublicKey::from(&priv_key);
+
+        Ok(KeyPair { priv_key, pub_key })
+    }
+
+    pub fn store_seedphrase(&self, seedphrase: &String, filename: &str) {
+        fs::write(filename, seedphrase).expect("failed to write seedphrase to file");
+    }
+
+    pub fn store_pub_key(&self, pub_key: RsaPublicKey) {
+        pub_key
+            .write_pkcs1_pem_file("pubkey.txt", Default::default())
+            .expect("failed to write public key to file");
+    }
+
+    pub fn generate_keypair_and_write_to_file(&self) -> Result<KeyPair, String> {
+        let seedphrase = self.generate_seedphrase();
+        self.store_seedphrase(&seedphrase, "seedphrase.txt");
+
+        let KeyPair = match self.generate_rsa_keypair(&seedphrase) {
+            Ok(x) => x,
+            Err(e) => return Err(e)
+        };
+
+        let keypair = self.generate_rsa_keypair(&seedphrase).unwrap();
+        let pub_key = keypair.pub_key.to_owned();
+        self.store_pub_key(pub_key);
+
+        Ok(keypair)
+    }
+}
+
+//#[test]
+/* fn test_from_seedphrase(){
+    let seedphrase = SeedPhrase::new().unwrap();
+
+    let key1 = Keygen::from_seedphrase(Some(&seedphrase)).unwrap().pub_key;
+    let key2 = Keygen::from_seedphrase(Some(&seedphrase)).unwrap().pub_key;
+
+    assert_eq!(key1, key2);
+} */
--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,8 @@
-use rand::{rngs::StdRng, SeedableRng};
-use random_word::Lang;
-use rsa::{pkcs1::EncodeRsaPublicKey, RsaPrivateKey, RsaPublicKey};
-use sha2::{Sha256, Digest};
-use std::fs;
+mod keygen;
+
+use keygen::Keygen;
 
 fn main() {
-    let pub_key = generate_pubkey();
-    let pem = pub_key
-        .to_pkcs1_pem(Default::default())
-        .expect("failed to serialize public key");
-
-    fs::write("pubkey.txt", &pem).expect("Unable to write to file");
-}
-
-fn generate_pubkey() -> RsaPublicKey {
-    //generate seed_phrase
-    let seed_phrase = generate_12_word_seed_phrase();
-
-    let priv_key = generate_rsa_from_seed(seed_phrase);
-    let pub_key = RsaPublicKey::from(&priv_key);
-
-    pub_key
-}
-
-fn generate_rsa_from_seed(seed_phrase: String) -> RsaPrivateKey {
-
-    // Generate a seed from the seed phrase
-    let mut hasher = Sha256::new();
-    hasher.update(seed_phrase);
-    let seed = hasher.finalize();
-
-    // Convert the seed into a byte array of length 32
-    let seed_array: [u8; 32] = seed.into();
-
-    // Use the seed to create a random number generator
-    let mut rng = StdRng::from_seed(seed_array);
-
-    // Generate the RSA private key
-    RsaPrivateKey::new(&mut rng, 2048).expect("failed to generate a key")
-}
-
-fn generate_12_word_seed_phrase() -> String {
-    let mut seed_phrase = String::from("");
-
-    for _ in 0..12 {
-        let word = random_word::gen(Lang::En);
-        seed_phrase.push_str(&word);
-        seed_phrase.push_str(" ");
-    }
-    fs::write("seedphrase.txt", &seed_phrase).expect("Unable to write to file");
-    seed_phrase
+    let keygen = Keygen::new();
+    let Keypair = keygen.generate_keypair_and_write_to_file();
 }
