638198283c1f5bbc9ef3c19d73ccc3bbb83ffe9a
Emil <klemmensen11@gmail.com>
2024-06-10 00:26:37 +0200

    added lib export of new function ...

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@ pub use rsa_keygen::generate_seedphrase;
 pub use rsa_keygen::keypair_from_seedphrase;
 pub use rsa_keygen::keypair_from_private_key;
 pub use rsa_keygen::generate_seedphrase_and_keypair;
+pub use rsa_keygen::seedphrase_from_password;
 pub use rsa_keygen::pkcs1_pem_from_priv_key;
 pub use rsa_keygen::pkcs1_pem_from_pub_key;
 pub use rsa_keygen::store_in_file;
